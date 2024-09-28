da0ce87db17c47023c96898e3a638f78015d67fd
Marcelo Lopes <marc01mcl@hotmail.com>
2024-09-28 11:00:44 -0300

    implementando as propriedades, herdando a classe smartphone e sobrescrevendo o método instalar aplicativo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "99999", modelo: "Nokia 3210", imei: "123456789", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "589564", modelo: "Iphone X", imei: "542895618", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
